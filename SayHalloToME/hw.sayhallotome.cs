89cd29a19c025b0b229ab5865d1c227ddaf3ea39
lyubo <lyubomir.iliev@mail.bg>
2017-06-09 22:55:23 +0300

    homework 1 and 2

---
+++ SayHalloToME/hw.sayhallotome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SayHalloToME
+{
+    class SayHalloToMe
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, what's your name darling");
+            string clientname = Console.ReadLine();
+            Console.WriteLine(clientname + " How old are you");
+            string clientage = Console.ReadLine();
+            Console.WriteLine("ohh i'm  "+ clientage +"  years old too");
+        }
+    }
+}
---
+++ homeworks/homework1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace homeworks
+{
+    class homework1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Today is:");
+            Console.WriteLine(DateTime.Now);
+
+        }
+    }
+}
